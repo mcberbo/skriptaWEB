bd23d3059173328ef827ce35ab1143348dfc7c25
mcberbo <59576062+mcberbo@users.noreply.github.com>
2020-07-08 14:35:53 +0200

    second commit

---
+++ resources/js/home.js
@@ -0,0 +1,49 @@
+document.querySelector('.link-home').addEventListener('click', function () {
+    document.getElementById('naslov').textContent = 'Dobrodošli na skripta.ba';
+    document.getElementById('content2').style.display = 'none';
+    document.getElementById('content1').style.display = 'block';
+    document.getElementById('content3').style.display = 'none';
+    document.getElementById('content4').style.display = 'none';
+    document.getElementById('content5').style.display = 'none';
+    document.getElementById('body1').style.backgroundImage = 'linear-gradient(to top, #5b63b7, #4a59a5, #394e93, #294481, #1a3a6f, #23305e, #25274e, #231f3e, #24172c, #1e111d, #15080f, #000000'
+})
+
+document.querySelector('.link-pogodnosti').addEventListener('click', function () {
+    document.getElementById('naslov').textContent = 'Pogodnosti';
+    document.getElementById('content1').style.display = 'none';
+    document.getElementById('content2').style.display = 'block';
+    document.getElementById('content3').style.display = 'none';
+    document.getElementById('content4').style.display = 'none';
+    document.getElementById('content5').style.display = 'none';
+    document.getElementById('body1').style.backgroundImage = 'linear-gradient(to bottom, #5b63b7, #4a59a5, #394e93, #294481, #1a3a6f, #23305e, #25274e, #231f3e, #24172c, #1e111d, #15080f, #000000'
+})
+document.querySelector('.link-fakulteti').addEventListener('click', function () {
+    document.getElementById('naslov').textContent = 'Fakulteti';
+    document.getElementById('content1').style.display = 'none';
+    document.getElementById('content2').style.display = 'none';
+    document.getElementById('content3').style.display = 'block';
+    document.getElementById('content4').style.display = 'none';
+    document.getElementById('content5').style.display = 'none';
+    document.getElementById('body1').style.backgroundImage = 'linear-gradient(to bottom, #5b63b7, #4a59a5, #394e93, #294481, #1a3a6f, #23305e, #25274e, #231f3e, #24172c, #1e111d, #15080f, #000000'
+
+})
+document.querySelector('.link-prijava').addEventListener('click', function () {
+    document.getElementById('naslov').textContent = 'Prijava';
+    document.getElementById('content1').style.display = 'none';
+    document.getElementById('content2').style.display = 'none';
+    document.getElementById('content3').style.display = 'none';
+    document.getElementById('content4').style.display = 'block';
+    document.getElementById('content5').style.display = 'none';
+    document.getElementById('body1').style.backgroundImage = 'linear-gradient(to bottom, #5b63b7, #4a59a5, #394e93, #294481, #1a3a6f, #23305e, #25274e, #231f3e, #24172c, #1e111d, #15080f, #000000'
+
+})
+document.querySelector('.link-registracija').addEventListener('click', function () {
+    document.getElementById('naslov').textContent = 'Registracija';
+    document.getElementById('content1').style.display = 'none';
+    document.getElementById('content2').style.display = 'none';
+    document.getElementById('content3').style.display = 'none';
+    document.getElementById('content4').style.display = 'none';
+    document.getElementById('content5').style.display = 'block';
+    document.getElementById('body1').style.backgroundImage = 'linear-gradient(to bottom, #5b63b7, #4a59a5, #394e93, #294481, #1a3a6f, #23305e, #25274e, #231f3e, #24172c, #1e111d, #15080f, #000000'
+
+})
